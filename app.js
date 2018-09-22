26b1819d4544642dac645e105a938323af448b75
Jorge Irizarry <jorge.l.irizarry@gmail.com>
2018-09-21 22:55:15 -0400

    add EP2 and EP3

--- app.js
+++ app.js
@@ -2,9 +2,75 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mysql = require("mysql")
+
+const body_parser = require("body-parser")
+
+// Middleware to get form data
+app.use(body_parser.urlencoded({extended: false}))
+
+// Serve static pages in the public folder
+app.use(express.static('./public'))
 
 app.use(morgan('short'))
 
+app.use("/user_create", (req,res) =>{
+    // console.log("Trying to create a new user...")
+    // console.log("How do we get the form data???")
+    // console.log("First name: " + req.body.create_first_name)
+    const first_name = req.body.create_first_name
+    const last_name = req.body.create_last_name
+
+    const query_string = "INSERT INTO users (first_name, last_name) VALUES (?,?)"
+    getConnection().query(query_string, [first_name, last_name], (err, results, fields)=>{
+        if(err){
+            console.log("Field to insert new user: "+ err)
+            res.sendStatus(500)
+        }
+        console.log("Inserted a new user with id: " , results.insertId);
+        res.end()
+    })
+    // res.end()
+})
+
+function getConnection(){
+    return mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "W@rld8-4",
+        database: "lbta_mysql"
+    })
+
+}
+
+// user route   
+app.get("/user/:id", (req, res) => {
+    console.log("Fetching user with id: " + req.params.id)
+    
+    const connection = getConnection();
+
+    const user_id = req.params.id
+    const query_string = "SELECT * FROM users WHERE id = ?"
+    connection.query(query_string, [user_id], (err, rows, fields) => {
+        if (err) {
+            console.log("Failed to query for user..." + err)
+            res.sendStatus(500)
+            // throw err
+            // res.end()
+            return
+        }
+        console.log("I think we fetched the users successfully")
+
+        const users = rows.map((row)=>{
+            return {firstName: row.first_name,
+                lastName: row.last_name
+            }
+        })
+        res.json(users)
+    })
+    
+    // res.end()
+})
 // root route
 app.get("/", (req, res) => {
     console.log("Responding to root route")
@@ -13,12 +79,25 @@ app.get("/", (req, res) => {
 
 // users route
 app.get("/users", (req, res) => {
-    var user1 = {firstName: "Lorena", lastName: "Sabathié"}
-    const user2 = {firstName: "Jorge", lastName: "Irizarry"}
-    res.json([user1, user2])
+    // Static users response
+    // const user1 = {firstName: "Lorena", lastName: "Sabathié"}
+    // const user2 = {firstName: "Jorge", lastName: "Irizarry"}
 
+    // TODO: Remember to refactor connection
+    const connection = getConnection()
+    const query_string = "SELECT * FROM users;"
+    
+    connection.query(query_string, (err, rows, fields)=>{
+        if (err) {
+            console.log('There was an error fetching the users... ' + err.toString())
+            res.sendStatus(500)
+            return
+        } else {
+            res.json(rows)
+        }
+    })
 
-
+    // res.json([user1, user2])
     // res.send("Nodemon auto updates when I save this file")
 })
 
